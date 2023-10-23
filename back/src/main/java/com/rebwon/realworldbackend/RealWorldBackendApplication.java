0c6afde9ee9dc4f968d0ff7ffc8c2f8f2043c90f
carmarolli <christophe.armarolli@4sh.fr>
2023-10-24 01:29:43 +0200

    Modify application for servlet container deployment (Tomcat)

--- back/src/main/java/com/rebwon/realworldbackend/RealWorldBackendApplication.java
+++ back/src/main/java/com/rebwon/realworldbackend/RealWorldBackendApplication.java
@@ -2,9 +2,10 @@ package com.rebwon.realworldbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RealWorldBackendApplication {
+public class RealWorldBackendApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RealWorldBackendApplication.class, args);
